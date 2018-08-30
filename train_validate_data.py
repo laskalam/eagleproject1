969bbeb569bcb364f39f253b7f6fa0063ec8442d
mika rafieferantsoa <rafieferantsoamika@gmail.com>
2018-08-30 12:14:45 +0200

    removed the seed but put it in main

--- train_validate_data.py
+++ train_validate_data.py
@@ -3,7 +3,6 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-np.seed = 123456789
 def train_validate_split(df, train_percent=.6, seed=None):
     """
     This function randomly divides data into 2 separate sets
