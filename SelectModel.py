0a80875b3b36ff4143abd2381ef9073f6fc129c0
mika rafieferantsoa <rafieferantsoamika@gmail.com>
2018-08-30 12:06:31 +0200

    function to select the model to train with

---
+++ SelectModel.py
@@ -0,0 +1,60 @@
+TYPE = 'RF', 'ABR', 'BR', 'GBR', 'OLS', 'SGD', 'HR', 'DTR', 'ETR'
+
+
+import sys
+import numpy as np
+import pandas as pd
+
+def GetRegressor(TYPE):
+    '''
+    Use:
+    GetRegressor(Type)
+    #Type should be one of the list above
+    Return:
+    regressor, regressorname, type of regressor
+    '''
+    if TYPE in ['RF', 'ABR', 'BR', 'GBR']:
+        TypeOfRegression = 'ensemble'
+    if TYPE in ['OLS', 'SGD', 'HR']:
+        TypeOfRegression = 'linear_model'
+    if TYPE in ['DTR', 'ETR']:
+        TypeOfRegression = 'tree'
+    #Ensemble
+    if TYPE == 'ABR':
+        regressor = 'AdaBoostRegressor' 
+        from sklearn.ensemble import AdaBoostRegressor as Regressor
+    if TYPE == 'BR':
+        regressor = 'BaggingRegressor'
+        from sklearn.ensemble import BaggingRegressor as Regressor
+    if TYPE == 'ETR':
+        regressor = 'ExtraTreesRegressor'
+        from sklearn.ensemble import ExtraTreesRegressor as Regressor
+    if TYPE == 'GBR':
+        regressor = 'GradientBoostingRegressor'
+        from sklearn.ensemble import GradientBoostingRegressor as Regressor
+    if TYPE == 'RF':
+        regressor = 'RandomForestRegressor'
+        from sklearn.ensemble import RandomForestRegressor as Regressor
+    #Linear Model
+    if TYPE == 'OLS':
+        regressor = 'LinearRegression' #Basic least square
+        from sklearn.linear_model import LinearRegression as Regressor
+    if TYPE == 'HR':
+        regressor = 'HuberRegressor' #Good for outliers
+        from sklearn.linear_model import HuberRegressor as Regressor
+    if TYPE == 'SGD':
+        regressor = 'SGRRegressor' #Good for outliers
+        from sklearn.linear_model import SGDRegressor as Regressor
+    #Tree algorithm
+    if TYPE == 'DTR':
+        regressor = 'DecisionTreeRegressor'
+        from sklearn.tree import DecisionTreeRegressor as Regressor
+    if TYPE == 'ETR':
+        regressor = 'ExtraTreeRegressor'#Extremely randomized tree
+        from sklearn.tree import ExtraTreeRegressor as Regressor
+
+    return Regressor, regressor , TypeOfRegression
+
+if __name__ == "__main__":
+    for t in TYPE:
+        print GetRegressor(t)
