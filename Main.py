2ed5bdf074bf47e2e48031e0c1cd0a1f6c9cd10e
mika rafieferantsoa <rafieferantsoamika@gmail.com>
2018-08-31 15:12:38 +0200

    still testing man

--- Main.py
+++ Main.py
@@ -19,28 +19,43 @@ data = '../data/train.csv'
 #train_data = pd.read_csv(data)
 #print list(train_data)
 #sys.exit()
-train_data = ProcessTheData(data, miss_threshold=MISS_THRESHOLD,\
+#sys.exit()
+train_data, CatNum_features, final_feature  = ProcessTheData(data, miss_threshold=MISS_THRESHOLD,
                                   corr_threshold=CORR_THRESHOLD)
-sys.exit()
 data = '../data/test.csv'
-test_data = ProcessTheData(data, miss_threshold=MISS_THRESHOLD,
-                                 corr_threshold=CORR_THRESHOLD)
+test_data = ProcessTheData(data, miss_threshold=MISS_THRESHOLD, corr_threshold=CORR_THRESHOLD,
+                                 final_features=final_feature,catnum_features=CatNum_features)
 #
 #TO BE CHANGED
-# Handle missing values, feature selection
-train_data.isnull().sum()[train_data.isnull().sum()>0]
-#Feature_dropped = ['PoolQC','Fence','MiscFeature','Alley','FireplaceQu']
-#train_data = train_data.drop(Feature_dropped, axesis=1)
-#test_data = test_data.drop(Feature_dropped, axis=1)
-model_predictors = ['LotArea','YearBuilt','1stFlrSF','2ndFlrSF','FullBath','BedroomAbvGr','TotRmsAbvGrd']
+#model_predictors = ['LotArea','YearBuilt','1stFlrSF','2ndFlrSF','FullBath','BedroomAbvGr','TotRmsAbvGrd']
+Test_ID = test_data.Id
+#X_test = test_data[model_predictors]
 
 Train, Validate = split(train_data, train_percent=TRAINING_PERCENT, seed=SEED)
-#
-X_train, Y_train = Train[model_predictors], Train.SalePrice
+
+sys.exit()
+#Y_train = np.log10(Train['SalePrice'])
+Y_train = Train['SalePrice']
+X_train = Train.drop(['Id', 'SalePrice'], axis = 1, inplace = True)#[model_predictors]
+
+#Y_validate = np.log10(Validate['SalePrice'])
+Y_validate = Validate['SalePrice']
+X_validate = Validate.drop(['Id', 'SalePrice'], axis = 1, inplace = True)#[model_predictors]
+#Drop NaN in price
+#Validate.dropna(axis=0)
+#Validate = Validate[~Validate.isin([np.nan, np.inf, -np.inf]).any(1)]
+#Train = Train[~Train.isin([np.nan, np.inf, -np.inf]).any(1)]
+#X_train = Train[model_predictors]
+#Y_train = Train.SalePrice
+#X_train.drop( ["Id", "SalePrice"], axis = 1, inplace = True)
+#_validate = Validate.copy()
+#_validate.drop(["Id", "SalePrice"], axis=1, inplace = True)
+#_validate[model_predictors]
+#Do some plots
 plt.clf()
 fig, axes = plt.subplots(3,3, sharex='col', sharey='row')
-x_lim = 0,800000
-y_lim = 0,800000
+x_lim = 4,7
+y_lim = 4,7
 k = -1
 for i, Model in enumerate(MODELS):
     Regressor, Name, Type = GetRegressor(Model)
@@ -48,14 +63,14 @@ for i, Model in enumerate(MODELS):
     regressor_.fit(X_train, Y_train)
     
     prediction0 = regressor_.predict(X_train)
-    prediction1 = regressor_.predict(Validate[model_predictors])
+    prediction1 = regressor_.predict(X_validate)
     
     
     color0 = 'blue'
     color1 = 'lightgreen'
     metric0 = metric(prediction0, Y_train)[0]
-    metric1 = metric(prediction1, Validate.SalePrice)[0]
-    
+    metric1 = metric(prediction1, Y_validate)[0]
+    Prediction = regressor_.predict(X_test)
     
     
     
@@ -63,10 +78,10 @@ for i, Model in enumerate(MODELS):
     j = i%3
     if j == 0:
         k += 1
-    axes[k,j].scatter(Y_train, prediction0, c = color0, marker = "s", label = "Training")
-    axes[k,j].scatter(Validate.SalePrice, prediction1, c = color1, marker = "s", label = "Validation")
-    axes[k,j].text(0.9, 0.2, '%.f'%metric0, color=color0, ha= 'right', va='bottom', transform=axes[k,j].transAxes)
-    axes[k,j].text(0.9, 0.1, '%.f'%metric1, color=color1, ha= 'right', va='bottom', transform=axes[k,j].transAxes)
+    axes[k,j].plot(Y_train, prediction0, '.', color = color0, label = "Training")
+    axes[k,j].plot(Y_validate, prediction1, '.', color = color1, label = "Validation")
+    axes[k,j].text(0.9, 0.2, '%.4f'%metric0, color=color0, ha= 'right', va='bottom', transform=axes[k,j].transAxes)
+    axes[k,j].text(0.9, 0.1, '%.4f'%metric1, color=color1, ha= 'right', va='bottom', transform=axes[k,j].transAxes)
     axes[k,j].text(0.01, 0.9, '%s'%Name, color='k', ha= 'left', va='top', fontsize=8, transform=axes[k,j].transAxes)
     #axes[k,j].set_title(Name)
     if k==2:
@@ -80,11 +95,15 @@ for i, Model in enumerate(MODELS):
     axes[k,j].set_xlim(x_lim)
     axes[k,j].set_ylim(y_lim)
 
-
+    output = 'output%d.csv'%i
+    #print Test_ID
+    #print Prediction
+    np.savetxt(output, np.c_[Test_ID, 10**Prediction], fmt=['%d', '%f'], delimiter=',', comments='', header='Id,SalePrice')
+    print output, 'saved'
     #file_name = '%s.pdf saved'%Name
     #plt.savefig('%s.pdf'%Name, bbox_inches='tight')
-file_name = 'All.pdf saved'
-plt.savefig('All.pdf', bbox_inches='tight')
+file_name = 'All1.pdf'
+plt.savefig(file_name, bbox_inches='tight')
 print (file_name, 'saved')
     #plt.show()
 
